3c1dba45bac45234ea07a1aa049862ca8c526aaa
Seonmi <soul012632@gmail.com>
2021-01-23 18:02:45 +0800

    react router dom practice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
+import "./index.css"
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Home() {
+  return (
+  <div>
+    <h2>Home</h2>
+    Home....
+  </div>
+    )
+}
+
+function Topics() {
+  return (
+  <div>
+    <h2>Topics</h2>
+     <ul>
+       <li><NavLink to="/topics/1">HTML</NavLink></li>
+       <li><NavLink to="/topics/2">JS</NavLink></li>
+       <li><NavLink to="/topics/3">React</NavLink></li>
+     </ul>
+     <Switch>
+       <Route path="/topics/1">
+          HTML is....
+       </Route>
+       <Route path="/topics/2">
+          JS is.....
+       </Route>
+       <Route path="/topics/3">
+          React is ....
+       </Route>
+     </Switch>
+  </div>
+    )
+}
+
+function Contact() {
+  return (
+  <div>
+    <h2>Contact</h2>
+    Contact....
+  </div>
+    )
+}
+
+function App() {
+  return (
+    <div>
+    <h3>Hello React Router Dom</h3>
+    <ul>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/topics">Topics</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
+    </ul>
+    <Switch>
+    <Route exact path="/"><Home></Home></Route>//exact는 매칭되는 주소만 불러오게 해줌
+    <Route path="/topics"><Topics></Topics></Route>
+    <Route path="/contact"><Contact></Contact></Route>
+    <Route path="/">Not found</Route>
+    </Switch>
+    </div>
+  )
+}
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter> ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
