527fd003a95fa7d0dd42e15fc1e82ac358e7e1ee
Seonmi <soul012632@gmail.com>
2021-01-23 19:04:46 +0800

    insert data automatically

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, NavLink, useParams} from 'react-router-dom'
 import "./index.css"
 import reportWebVitals from './reportWebVitals';
 
@@ -13,26 +13,51 @@ function Home() {
     )
 }
 
+let contents = [
+    {id:1, title:'HTML', description:'HTML is...'},
+    {id:2, title:'JS', description:'JS is...'},
+    {id:3, title:'React', description:'React is...'}
+]
+
+function Topic() {
+  let params = useParams();
+  let topic_id = params.topic_id;
+  let selected_topic = {
+      title:"Sorry",
+      description:"Not found"
+  }
+  for(let i = 0; i < contents.length; i++) {
+    if(contents[i].id === Number(topic_id)) {
+       selected_topic = contents[i];
+      break;
+    
+    }
+  }
+  console.log('params', params, params.topic_id);
+  return (
+   <div>
+    <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+   </div>
+  );
+}
 function Topics() {
+  let lis = [];
+    for(let i = 0; i < contents.length; i++) {
+      lis.push(<li key={contents[i].id}><NavLink to={"/topics/"+contents[i].id}>{contents[i].title}
+                   </NavLink>
+               </li>)
+    }
   return (
   <div>
     <h2>Topics</h2>
      <ul>
-       <li><NavLink to="/topics/1">HTML</NavLink></li>
-       <li><NavLink to="/topics/2">JS</NavLink></li>
-       <li><NavLink to="/topics/3">React</NavLink></li>
+       {lis}
      </ul>
-     <Switch>
-       <Route path="/topics/1">
-          HTML is....
-       </Route>
-       <Route path="/topics/2">
-          JS is.....
-       </Route>
-       <Route path="/topics/3">
-          React is ....
-       </Route>
-     </Switch>
+     <Route path="/topics/:topic_id">
+       <Topic></Topic>
+     </Route>
+
   </div>
     )
 }
@@ -56,7 +81,7 @@ function App() {
       <li><NavLink to="/contact">Contact</NavLink></li>
     </ul>
     <Switch>
-    <Route exact path="/"><Home></Home></Route>//exact는 매칭되는 주소만 불러오게 해줌
+    <Route exact path="/"><Home></Home></Route> //exact는 매칭되는 주소만 불러오게 해줌
     <Route path="/topics"><Topics></Topics></Route>
     <Route path="/contact"><Contact></Contact></Route>
     <Route path="/">Not found</Route>
